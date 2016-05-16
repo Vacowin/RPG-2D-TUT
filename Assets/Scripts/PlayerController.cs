727a4cce8c7abfdf1d6eb966aa383b5447b1ed38
Nguyen Van <nvan@upei.ca>
2016-05-16 20:55:30 -0300

    video 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movingSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * movingSpeed * Time.deltaTime,0.0f,0.0f));
+        }
+
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+        {
+            transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * movingSpeed * Time.deltaTime, 0.0f));
+        }
+    }
+}
