ff8b076ba7007193339c60e06e9596ab9b7643f6
Nguyen Van <Nguyen Van>
2016-06-05 21:56:14 -0300

    video 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,11 @@ public class PlayerController : MonoBehaviour {
 
     public float movingSpeed;
 
+	private Animator anim;
+
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -15,11 +17,15 @@ public class PlayerController : MonoBehaviour {
 	    if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * movingSpeed * Time.deltaTime,0.0f,0.0f));
+			Debug.Log ("ahihi");
         }
 
         if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
         {
             transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * movingSpeed * Time.deltaTime, 0.0f));
         }
+
+		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+		anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
     }
 }
