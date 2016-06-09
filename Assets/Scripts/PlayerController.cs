89a231b61c2e96c38d86d7b6fa84b7098caba750
Nguyen Van <Nguyen Van>
2016-06-08 23:38:21 -0300

    video 4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float movingSpeed;
-
+	private bool playerMoving;
+	private Vector2 lastMove;
 	private Animator anim;
 
 	// Use this for initialization
@@ -14,18 +15,26 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		playerMoving = false;
+
 	    if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * movingSpeed * Time.deltaTime,0.0f,0.0f));
-			Debug.Log ("ahihi");
+			playerMoving = true;
+			lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0);
         }
 
         if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
         {
             transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * movingSpeed * Time.deltaTime, 0.0f));
+			playerMoving = true;
+			lastMove = new Vector2 (0, Input.GetAxisRaw ("Vertical"));
         }
 
 		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
 		anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+		anim.SetBool ("PlayerMoving", playerMoving);
+		anim.SetFloat ("LastMoveX", lastMove.x);
+		anim.SetFloat ("LastMoveY", lastMove.y);
     }
 }
