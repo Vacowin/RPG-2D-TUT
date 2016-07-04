2b2c43ac9a84ab80682762671de2d940f2d88c0a
Nguyen Van <Nguyen Van>
2016-07-03 21:58:24 -0300

    Video 8: rigidbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,14 @@ public class PlayerController : MonoBehaviour {
     public float movingSpeed;
 	private bool playerMoving;
 	private Vector2 lastMove;
+
 	private Animator anim;
+	private Rigidbody2D rigidBody2D;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
+		rigidBody2D = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
@@ -19,17 +22,27 @@ public class PlayerController : MonoBehaviour {
 
 	    if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * movingSpeed * Time.deltaTime,0.0f,0.0f));
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * movingSpeed * Time.deltaTime,0.0f,0.0f));
+			rigidBody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movingSpeed, rigidBody2D.velocity.y);
 			playerMoving = true;
 			lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0);
         }
 
         if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
         {
-            transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * movingSpeed * Time.deltaTime, 0.0f));
+            //transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * movingSpeed * Time.deltaTime, 0.0f));
+			rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, Input.GetAxisRaw("Vertical") * movingSpeed);
 			playerMoving = true;
 			lastMove = new Vector2 (0, Input.GetAxisRaw ("Vertical"));
         }
+
+		if (Mathf.Abs (Input.GetAxisRaw ("Horizontal")) < 0.5f) {
+			rigidBody2D.velocity = new Vector2(0.0f, rigidBody2D.velocity.y);
+		}
+
+		if (Mathf.Abs (Input.GetAxisRaw ("Vertical")) < 0.5f) {
+			rigidBody2D.velocity = new Vector2 (rigidBody2D.velocity.x, 0.0f);
+		}
 
 		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
 		anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
